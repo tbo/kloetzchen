78f52a23290efb36830bb6f52650196352c77d75
Dave Brotherstone <davegb@pobox.com>
2015-02-21 16:11:50 +0100

    Stable tower of 15 rows

--- app/createCuboid.js
+++ app/createCuboid.js
@@ -1,21 +1,21 @@
 
-const CUBE_HEIGHT = 0.5;
-const CUBE_WIDTH = 1.0;
+const CUBE_HEIGHT = 1;
+const CUBE_WIDTH = 2.0;
+const CUBE_LENGTH = 3.0;
 
 function createCuboid(row, position) {
     var x,y,z, zRotation = 0.0;
 
     if (row % 2 === 0) {
-        zRotation = Math.PI;
-        x = 0.0;
-        y = position * CUBE_WIDTH;
-    } else {
         x = position * CUBE_WIDTH;
         y = 0.0;
+    } else {
+        zRotation = Math.PI / 2;
+        x = (CUBE_LENGTH / 2) + (CUBE_WIDTH / 4.0);
+        y = (position - 1)* CUBE_WIDTH;
     }
 
-    z = row * CUBE_HEIGHT;
-
+    z = (row  * CUBE_HEIGHT) + (CUBE_HEIGHT / 2.0);
 
     return { x, y, z, zRotation };
 
--- app/initialise.js
+++ app/initialise.js
@@ -1,12 +1,12 @@
 
 var createCuboid = require('./createCuboid');
 function initialise(gameState) {
-    gameState.addCuboid(createCuboid(0, 0));
-    gameState.addCuboid(createCuboid(0, 1));
-    gameState.addCuboid(createCuboid(0, 2));
-    gameState.addCuboid(createCuboid(1, 0));
-    gameState.addCuboid(createCuboid(1, 1));
-    gameState.addCuboid(createCuboid(1, 2));
+    for(let row = 0; row < 15; ++row ) {
+        gameState.addCuboid(createCuboid(row, 0));
+        gameState.addCuboid(createCuboid(row, 1));
+        gameState.addCuboid(createCuboid(row, 2));
+
+    }
 }
 
 module.exports = initialise;
